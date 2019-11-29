a143b125d7bbb36f8f4b8b3b7da10530fe5c88bc
Bogdan Seniuc <bogdan@bogdan.site>
2019-11-29 18:36:33 +0200

    Add implementation

---
+++ pool.go
@@ -0,0 +1,68 @@
+package errpool // import "go.bog.dev/errpool"
+
+import (
+	"context"
+	"golang.org/x/sync/errgroup"
+)
+
+type Pool struct {
+	g       *errgroup.Group
+	ctx     context.Context
+	workers int
+	tasks   chan func(context.Context) error
+	closed  bool
+}
+
+func Unbounded(ctx context.Context) *Pool {
+	return WithNumWorkers(ctx, -1)
+}
+
+func WithNumWorkers(ctx context.Context, workers int) *Pool {
+	g, ctx := errgroup.WithContext(ctx)
+	tasks := make(chan func(context.Context) error)
+	pool := &Pool{
+		g:       g,
+		ctx:     ctx,
+		workers: workers,
+		tasks:   tasks,
+	}
+	for i := 0; i < workers; i++ {
+		g.Go(pool.worker)
+	}
+	return pool
+}
+
+func (p *Pool) worker() error {
+	var err error
+	for f := range p.tasks {
+		if err == nil {
+			err = f(p.ctx)
+		}
+	}
+	return err
+}
+
+func (p *Pool) Context() context.Context {
+	return p.ctx
+}
+
+func (p *Pool) Go(f func(ctx context.Context) error) {
+	if p.workers <= 0 {
+		select {
+		case p.tasks <- f:
+		default:
+			p.g.Go(p.worker)
+			p.tasks <- f
+		}
+	} else {
+		p.tasks <- f
+	}
+}
+
+func (p *Pool) Wait() error {
+	if !p.closed {
+		p.closed = true
+		close(p.tasks)
+	}
+	return p.g.Wait()
+}
